fc6d2934e8e222389777fd6f38210fe0601da089
xnrado <xnrad123@gmail.com>
2024-11-29 09:57:59 +0100

    navbar & others

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,7 @@
 <x-app-layout>
+    <x-slot name="header">
+        Dashboard
+    </x-slot>
     <div id="wrapper" class="h-full flex flex-col">
         <h1>Tytuł</h1>
         <div id="canvas-wrapper" class="h-full w-4/5 bg-amber-700 overflow-hidden self-center">
